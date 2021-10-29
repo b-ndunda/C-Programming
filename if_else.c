51197856e5883ae54f42f0cb8fd8980f7c9ef7cb
b-ndunda <70506956+b-ndunda@users.noreply.github.com>
2021-10-29 14:01:41 +0300

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+	printf("Enter value of a\n");
+	scanf("%d",&a);
+	if (a>5)
+	{
+		printf("a is greater than 5");
+	}
+    else
+	{
+		printf("a is less than 5");
+	}
+	
+    return 0;
+}
